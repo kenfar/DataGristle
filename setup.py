364856c05ffabfc8433fc62af92843e7e78c2070
Ken Farmer <kenfar@gmail.com>
2013-05-03 22:50:18 -0600

    incremented version to 0.49

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 
-version = "0.48"
+version = "0.49"
 DESCRIPTION      = 'A toolbox and library of ETL & data analysis tools'
 LONG_DESCRIPTION = open('README.txt').read()
 
