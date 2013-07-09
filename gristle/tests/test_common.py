f8d38d5b62f7fed09e5a72b227119c9ddc97e5e3
Ken Farmer <kenfar@gmail.com>
2013-07-08 20:55:18 -0600

    replaced unittest code with pytest

--- gristle/tests/test_common.py
+++ gristle/tests/test_common.py
@@ -9,20 +9,13 @@ import tempfile
 import random
 import atexit
 import shutil
-try:
-    import unittest2 as unittest
-except ImportError:
-    import unittest
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 import gristle.common  as mod
 
 
-class TestFunctions(unittest.TestCase):
-    def setUp(self):
-        pass
-    def tearDown(self):
-        pass
+class TestFunctions():
+
     def test_get_common_key(self):
         d = {'car': 7, 'truck':8, 'boat':30, 'plane': 5}
-        self.assertEqual(mod.get_common_key(d), ('boat',0.6))
+        assert mod.get_common_key(d) == ('boat',0.6)
