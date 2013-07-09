068b803b99d2d70ef889ce8a5dcbadfda55de841
Ken Farmer <kenfar@gmail.com>
2013-07-08 23:26:26 -0600

    converted unittest to pytest

--- gristle/tests/test_simplesql.py
+++ gristle/tests/test_simplesql.py
@@ -314,9 +314,4 @@ class AnimalTools(simplesql.TableTools):
 
 
 
-#if __name__ == "__main__":
-    #runner = unittest.TextTestRunner(verbosity=2)
-    #runner.run(suite())
-#   unittest.main(suite())
- 
 
