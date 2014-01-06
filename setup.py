9e9b0aab56b28d5ff0d00169c907efb4c73adfdc
Ken Farmer <kenfar@gmail.com>
2014-01-05 19:17:37 -0700

    updated for v0.53

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 
-version = "0.52"
+version = "0.53"
 DESCRIPTION      = 'A toolbox and library of ETL & data analysis tools'
 LONG_DESCRIPTION = open('README.txt').read()
 
