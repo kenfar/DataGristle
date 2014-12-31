dff3687bcf4ce6b11a288bcce000804bf92929fc
Ken <ken.farmer@protectwise.com>
2014-12-30 17:13:30 -0700

    Added tests for colnames to offsets function

--- gristle/tests/test_common.py
+++ gristle/tests/test_common.py
@@ -9,13 +9,54 @@ import tempfile
 import random
 import atexit
 import shutil
+import pytest
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 import gristle.common  as mod
 
 
-class TestFunctions():
+class TestFunctions(object):
 
     def test_get_common_key(self):
         d = {'car': 7, 'truck':8, 'boat':30, 'plane': 5}
         assert mod.get_common_key(d) == ('boat',0.6)
+
+
+class Test_colnames_to_coloff0(object):
+
+    def test_empty_lists(self):
+        assert mod.colnames_to_coloff0([], []) == []
+
+    def test_col_numbers_only(self):
+        assert mod.colnames_to_coloff0([], [0,3,5]) == [0,3,5]
+
+    def test_string_lookuplist_int_namelist(self):
+        assert mod.colnames_to_coloff0(['0','1','2'], [0]) == [0]
+
+    def test_int_lookuplist_string_namelist(self):
+        assert mod.colnames_to_coloff0([0,1,2], ['0']) == [0]
+
+    def test_col_numbers_strings_only(self):
+        assert mod.colnames_to_coloff0([], ['0','3','5']) == [0,3,5]
+
+    def test_col_all_names_only(self):
+        assert mod.colnames_to_coloff0(['foo', 'bar', 'baz'], ['foo', 'bar', 'baz']) == [0,1,2]
+
+    def test_col_some_names_only(self):
+        assert mod.colnames_to_coloff0(['foo', 'bar', 'baz'], ['bar']) == [1]
+
+    def test_col_mix_of_names_and_numbers(self):
+        assert mod.colnames_to_coloff0(['foo', 'bar', 'baz'], [0, 'bar', 'baz']) == [0,1,2]
+
+    def test_error_name_not_found(self):
+        with pytest.raises(KeyError):
+            assert mod.colnames_to_coloff0(['foo', 'bar', 'baz'], ['gorilla'])
+
+    def test_error_number_not_found(self):
+        with pytest.raises(KeyError):
+            assert mod.colnames_to_coloff0(['foo', 'bar', 'baz'], [5])
+
+
+
+
+
